7b7dc362f24baa497fabf468e1980ebe10849152
sukanm <andrewmott@me.com>
2024-12-06 07:37:23 +0000

    Python Code Examples

---
+++ Day1/examples.py
@@ -0,0 +1,46 @@
+#print("Hello World!")
+
+#This code prints your name
+#print("Hello " + input("What is your name?"))
+
+#print(len(input("What is your name?")))
+
+print ("Hello"[0])
+
+print("123"+"456")
+
+print (123+456)
+#integer
+print (123_456_789)
+#float
+print(3.14)
+#boolean
+print(True)
+print(False)
+
+print(int("123")+int("456"))
+# int()
+# float()
+# string()
+# bool()
+
+bmi=84/1.65 ** 2
+print(bmi)
+print(int(bmi))
+print(round(bmi))
+
+score = 0 
+score +=1
+print(score)
+score -=1
+print(score)
+score *=1
+print(score)
+score /=1
+print(score)
+#f strings
+print("Your score is "+ str(score))
+
+print(f"Your score is {score}")
+
+
---
+++ Day3/examples.py
@@ -0,0 +1,65 @@
+import random
+# height = int(input("how tall are you? "))
+
+# if height > 120:
+#     print("You can ride the rollercoaster")
+# elif height == 120:
+#     print("You are perfect")
+# else:
+#     print("You are short")
+
+# age = int(input("how old are you? "))
+
+# if age >= 45 and age<=55:
+#     print("have a free ride!")
+
+
+
+# random_integer = random.randint( 1,10)
+# print(random_integer)
+
+# states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+# print(states_of_america[2])
+
+# dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
+
+# print(dirty_dozen[random.randint(0,11)])
+
+# for vegetable in dirty_dozen:
+#     print(vegetable)
+
+# scores = [random.randint(1, 100) for _ in range(100)]
+
+# sum = 0
+# for score in scores:
+#     sum += score
+
+# print(sum)
+
+# max_score = 0
+# for score in scores:
+#     if score > max_score:
+#         max_score=score
+
+# print(max_score)
+
+# #range
+# total = 0
+# for num in range(1,101):
+#     total += num
+# print(total)
+
+# def my_function():
+#     print("hello")
+#     print("Bye")
+
+# my_function()
+
+num=0
+i=0
+while i < 10:
+    i+=1
+    num=i
+print(num)
+
